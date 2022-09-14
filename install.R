7531ef408aab39121669b54f5e4cfb56b64f0aea
jacob8776 <jacob.wynne.776@gmail.com>
2022-09-14 11:19:54 -0700

    updated output files from new run

--- install.R
+++ install.R
@@ -1,15 +1,26 @@
 ## Packages needed to run sunapee_LER_projections
 install.packages("remotes")
 remotes::install_github("GLEON/rLakeAnalyzer")
-remotes::install_github("aemon-j/GLM3r", ref = "v3.1.1")
+#remotes::install_github("aemon-j/GLM3r", ref = "v3.1.1")
 remotes::install_github("USGS-R/glmtools", ref = "ggplot_overhaul")
-remotes::install_github("aemon-j/FLakeR", ref = "inflow")
-remotes::install_github("aemon-j/GOTMr")
-remotes::install_github("aemon-j/gotmtools", ref = "yaml")
-remotes::install_github("aemon-j/SimstratR", ref = "v2.4")
-remotes::install_github("aemon-j/MyLakeR") 
+#remotes::install_github("aemon-j/FLakeR", ref = "inflow")
+
+#remotes::install_github("aemon-j/GOTMr")
+remotes::install_github("aemon-j/gotmtools", ref = 'yaml')
+#remotes::install_github("aemon-j/SimstratR", ref = "v2.4")
+#remotes::install_github("aemon-j/MyLakeR") 
 remotes::install_github("tadhg-moore/LakeEnsemblR", ref = "sunp-isimip")
+
+remotes::install_github("aemon-j/FLakeR@LakeEnsemblR_v1.0.0")
+remotes::install_github("aemon-j/GLM3r@LakeEnsemblR_v1.0.0")
+remotes::install_github("aemon-j/GOTMr@LakeEnsemblR_v1.0.0")
+#remotes::install_github("aemon-j/SimstratR@LakeEnsemblR_v1.0.0")
+remotes::install_github("aemon-j/MyLakeR@LakeEnsemblR_v1.0.0")
+
+
+
 install.packages("ggplot2")
+install.packages("car")
 install.packages("ggpubr")
 install.packages("dplyr") 
 install.packages("rLakeAnalyzer")
@@ -23,3 +34,6 @@ install.packages("plotrix")
 install.packages("here")
 install.packages("pacman")
 install.packages("ncdf4")
+
+install.packages("tidyverse")
+install.packages("ggthemes")
